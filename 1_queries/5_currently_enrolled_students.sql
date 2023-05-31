be890be34ecc8b7ef5f1143158632a6071612aac
Tamara Tell <2telltamara@gmail.com>
2023-05-30 20:04:00 -0400

    add query for currently enrolled students

---
+++ 1_queries/5_currently_enrolled_students.sql
@@ -0,0 +1,12 @@
+-- A student's end date will be NULL when they are currently enrolled in Bootcamp.
+
+-- Instruction
+-- Get all of the students currently enrolled.
+
+-- Get their name, id, and cohort_id.
+-- Order them by cohort_id.
+
+SELECT name, id, cohort_id
+FROM students
+WHERE end_date IS NULL
+ORDER BY cohort_id;
