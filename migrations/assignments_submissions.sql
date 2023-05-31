0c97f96badeacc7f6fac042a15c6d20e180bb804
Tamara Tell <2telltamara@gmail.com>
2023-05-30 20:21:09 -0400

    create tables for assignments and assignment submissions

---
+++ migrations/assignments_submissions.sql
@@ -0,0 +1,33 @@
+-- An assignment will have the following attributes:
+
+-- id: A unique identifier
+-- name: The name of the assignment
+-- content: The written content body of the assignment
+-- day: The day that the assignment appears on
+-- chapter: The order that the assignment will appear in the day.
+-- duration: The average time it takes a student to finish
+
+-- An assignment_submission will have the following attributes:
+
+-- id: A unique identifier
+-- assignment_id: The id of the assignment
+-- student_id: The id of the student
+-- duration: The time it took the student to complete the assignment
+-- submission_date: The date is was submitted
+
+CREATE TABLE assignments (
+id SERIAL PRIMARY KEY NOT NULL,
+name VARCHAR NOT NULL,
+content TEXT,
+day INTEGER,
+chapter INTEGER,
+duration INTERVAL
+);
+
+CREATE TABLE assignment_submission (
+id SERIAL PRIMARY KEY NOT NULL,
+assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+duration INTERVAL,
+submission_date DATE
+);
